157340271f126c14913f4ee3a28b1a2ec1ae7f9f
wsy <wsy8768236@163.com>
2019-10-16 15:17:29 +0800

    多线程带返回值的实现方法

--- javaweb/src/com/bjx/wsy/CustomThread.java
+++ javaweb/src/com/bjx/wsy/CustomThread.java
@@ -10,6 +10,9 @@
  */
 package com.bjx.wsy;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -21,6 +24,7 @@ import java.util.concurrent.locks.ReentrantLock;
  * @create 2019-10-11
  * @since 1.0.0
  */
+
 class SellSomething {
     private int count = 15;
     Lock lock = new ReentrantLock();
@@ -39,10 +43,18 @@ class SellSomething {
     }
 
 }
+class Mythread implements Callable<Integer> {
+
+    @Override
+    public Integer call() throws Exception {
+        System.out.println(Thread.currentThread().getName()+"i’m coming");
+        return 1024;
+    }
+}
 
 public class CustomThread {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         SellSomething sellSomething = new SellSomething();
         //java 8新特性匿名内部类调用单一接口的方式
         /**这样的使用效果使用有前提条件，该接口方法只有一个方法
@@ -59,6 +71,15 @@ public class CustomThread {
         new Thread(()-> {
             for (int i=0;i<20;i++) sellSomething.Sell();
         },"C").start();
-
+        //实现多线程的第二种方式
+        FutureTask<Integer> futureTask = new FutureTask<Integer>(new Mythread());
+        //new一个匿名的类，因为futureTask实现了Runnable接口所以可以这样写
+        new Thread(futureTask,"A").start();
+        //获取线程的返回值
+        Integer integer = futureTask.get();
+        System.out.println("integer = " + integer);
     }
+    //实现多线程的第三种方式显示callable接口
+
+
 }
